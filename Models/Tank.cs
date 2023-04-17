890e0f31c462356fbe2444887cfaa70a4f9068f5
Julius <Julius@DESKTOP-PH315HF>
2023-04-17 21:01:34 +0200

    Add project files.

---
+++ Models/Tank.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+namespace Tanks.Models
+{
+    public enum Direction
+    {
+        North,
+        East,
+        South,
+        West
+    }
+
+    public class Tank
+    {
+        public int Health { get; set; } = 3;
+        public int Range { get; set; } = 1;
+        public Direction Direction { get; set; } = Direction.North;
+    }
+
+
+public static class TankEndpoints
+{
+	public static void MapTankEndpoints (this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/v1/Tank");
+
+        group.MapGet("/", () =>
+        {
+            return new Tank();
+        })
+        .WithName("GetAllTanks");
+
+        group.MapGet("/{id}", (int id) =>
+        {
+            //return new Tank { ID = id };
+        })
+        .WithName("GetTankById");
+
+        group.MapPut("/{id}", (int id, Tank input) =>
+        {
+            return TypedResults.NoContent();
+        })
+        .WithName("UpdateTank");
+
+        group.MapPost("/", (Tank model) =>
+        {
+            //return TypedResults.Created($"/Tanks/{model.ID}", model);
+        })
+        .WithName("CreateTank");
+
+        group.MapDelete("/{id}", (int id) =>
+        {
+            //return TypedResults.Ok(new Tank { ID = id });
+        })
+        .WithName("DeleteTank");
+    }
+}}
+    
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Tanks.Controllers;
+using Tanks.Models;
+
+namespace Tanks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.MapTankEndpoints();
+
+            app.Run();
+        }
+    }
+}
