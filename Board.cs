9bb43b982dfcf8f7fc64ac0faefb0173e407060c
Julius de Boer <JuliusDeBoer@users.noreply.github.com>
2023-04-19 14:37:13 +0200

    Add mutex

--- Board.cs
+++ Board.cs
@@ -1,9 +1,45 @@
-﻿using System.Collections;
+﻿using Microsoft.CodeAnalysis.Elfie.Diagnostics;
+using Tanks.Models;
 
 namespace Tanks
 {
-    public static class Board
+    public static class BoardMutex
     {
-        public static Hashtable Map { get; set; } = new Hashtable();
+        private static readonly Mutex _mutex = new();
+        private static Board _board = new();
+        public static Board Board
+        {
+            get
+            {
+                _mutex.WaitOne();
+                Board content = _board;
+                _mutex.ReleaseMutex();
+                return content;
+            }
+            set
+            {
+                _mutex.WaitOne();
+                _board = value;
+                _mutex.ReleaseMutex();
+            }
+        }
+    }
+
+    public class Board
+    {
+        public Dictionary<int, Models.Tank> Tanks { get; set; } = new Dictionary<int, Models.Tank>();
+
+        int GetTankByPosition(Models.Position position)
+        {
+            foreach (KeyValuePair<int, Tank> pair in Tanks)
+            {
+                if(pair.Value.Position == position)
+                {
+                    return pair.Key;
+                }
+            }
+
+            return 0;
+        }
     }
 }
