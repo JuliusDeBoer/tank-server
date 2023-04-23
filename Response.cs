4ce578dbe62396735a85b7cf57272b8151221b7a
Julius de Boer <JuliusDeBoer@users.noreply.github.com>
2023-04-23 16:56:12 +0200

    Move ERR_BAD_ARGUMENTS to general errors

--- Response.cs
+++ Response.cs
@@ -23,9 +23,11 @@
             return TypedResults.BadRequest(response);
         }
 
+        // General errors
+        public static readonly Response ERR_BAD_ARGUMENTS = new("ERR_BAD_ARGUMENTS", "Arguments where invalid");
+
         // Tanks errors
         public static readonly Response ERR_NO_SUCH_TANK = new("ERR_NO_SUCH_TANK", "Tank was not found");
-        public static readonly Response ERR_BAD_ARGUMENTS = new("ERR_BAD_ARGUMENTS", "Arguments where invalid");
         public static readonly Response ERR_NOT_ENOUGH_ACTION_POINTS = new("ERR_NOT_ENOUGH_ACTION_POINTS", "Tank does not have enough action points");
         public static readonly Response ERR_MAX_LEVEL_REACHED = new("ERR_MAX_LEVEL_REACHED", "Tank is already at the highest level");
         public static readonly Response ERR_MAX_TANKS_REACHED = new("ERR_MAX_TANKS_REACHED", "Maximum number of tanks was reaced");
